a76670d9a4ff5e62fa636b2d87367de08f10e5a6
qxw528 <qxw528@163.com>
2017-03-15 15:45:53 +0800

    1.dic import namespace file 2.create the enum parent class

---
+++ app/com/dic/qiusir/constant/Enumeration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiusir
+ * Date: 3/15/17
+ * Time: 11:21 AM
+ */
+
+namespace com\dic\qiusir\constant;
+
+
+class Enumeration
+{
+
+}
---
+++ bootstrap/app.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiusir
+ * Date: 3/15/17
+ * Time: 11:13 AM
+ */
